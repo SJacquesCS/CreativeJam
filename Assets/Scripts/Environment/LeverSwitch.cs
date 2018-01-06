dd6ba45653b205c0ebdd44c2fcf2363d26b9f9f4
NAD.QC.CA\xpro <aymenamous@gmail.com>
2018-01-06 10:33:10 -0500

    Added interaction with switches

--- Assets/Scripts/Environment/LeverSwitch.cs
+++ Assets/Scripts/Environment/LeverSwitch.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LeverSwitch : MonoBehaviour {
-    public GameObject mLinkedDoor;
-
-    public void ActivateSwitch()
-    {
-        GetComponent<Animator>().enabled = true;
-    }
+    public GameObject mLinkedWaterfall;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        ActivateSwitch();
+        if (collision.gameObject.tag == "mage")
+        {
+            transform.Rotate(new Vector3(0, 180));
+            transform.position = new Vector3(transform.position.x + 0.75f, transform.position.y);
+
+            mLinkedWaterfall.GetComponent<Waterfall>().Die();
+        }
     }
 }
