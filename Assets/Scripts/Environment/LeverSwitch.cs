18c347916d59d186a1dce16bcef9f890110f45f3
NAD.QC.CA\xpro <s-jacques@live.com>
2018-01-06 11:52:18 -0500

    Added post signs Added interactions with torch

--- Assets/Scripts/Environment/LeverSwitch.cs
+++ Assets/Scripts/Environment/LeverSwitch.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class LeverSwitch : MonoBehaviour {
     public GameObject mLinkedWaterfall;
+    public GameObject mOtherSwitch;
+
+    private bool mIsTriggered = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "mage")
+        if (collision.gameObject.tag == "mage" && !mIsTriggered)
         {
+            mIsTriggered = true;
+
+            mOtherSwitch.GetComponent<LeverSwitch>().setTriggered();
+
             transform.Rotate(new Vector3(0, 180));
             transform.position = new Vector3(transform.position.x + 0.75f, transform.position.y);
 
             mLinkedWaterfall.GetComponent<Waterfall>().Die();
         }
+    }
+
+    public void setTriggered()
+    {
+        mIsTriggered = true;
+
+        transform.Rotate(new Vector3(0, 180));
+        transform.position = new Vector3(transform.position.x + 0.75f, transform.position.y);
     }
 }
