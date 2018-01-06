ab43045757860ec407044f48bcde7763033e661e
NAD.QC.CA\xpro <steven.dien@outlook.com>
2018-01-06 08:26:07 -0500

    - Initial Commit - Added a placeholder menu scene, stage 01 (tutorial) scene and test scene - Added the player 1 (wizard) with movement controls (left, right, jump) and their animations - The world will be pitch black during live gameplay (only light sources ie: fire, can illuminate the world)

---
+++ Assets/Scripts/Characters/WizardMovement.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WizardMovement : MonoBehaviour {
+	
+    public float mWalkSpeed;
+    public float mRunSpeed;
+    public float mJumpForce;
+
+	public Transform mTopLeftPosition;
+	public Transform mBottomRightPosition;
+	public LayerMask mGroundLayer;
+
+    bool mRunning;
+    bool mWalking;
+    bool mGrounded;
+    bool mFalling;
+	
+	Animator mAnimator;
+	Rigidbody2D mRigidBody2D;
+    Transform mSpriteChild;
+
+	void Awake ()
+	{
+		mAnimator = GetComponentInChildren<Animator>();
+        mRigidBody2D = GetComponent<Rigidbody2D>();
+        mSpriteChild = transform.Find ("WizardSprite");
+	}
+
+	void Update()
+	{
+		CheckFalling ();
+		CheckGrounded ();
+	}
+		
+	void FixedUpdate ()
+	{
+		Move ();
+		
+		mAnimator.SetBool("IsWalking", mWalking);
+        mAnimator.SetBool("IsRunning", mRunning);
+	}
+
+	void Move ()
+	{
+		float moveLeft = Input.GetAxisRaw("Left");
+		float moveRight = Input.GetAxisRaw ("Right");
+
+		Vector3 directionLeft = new Vector3(moveLeft, 0.0f, 0.0f);
+		Vector3 directionRight = new Vector3(moveRight, 0.0f, 0.0f);
+
+		float movementSpeed = mWalkSpeed;
+
+		if (Input.GetKey (KeyCode.LeftShift) && mWalking)
+		{
+			mRunning = true;
+			movementSpeed = mRunSpeed;
+		}
+		else
+			mRunning = false;
+		
+		if (Input.GetKey (KeyCode.A))
+		{
+			FaceDirection ((Vector2)directionRight);
+			transform.position -= directionLeft * movementSpeed * Time.deltaTime;
+			mWalking = true;
+		}
+			
+		if (Input.GetKey (KeyCode.D))
+		{
+			FaceDirection ((Vector2)directionRight);
+			transform.position += directionRight * movementSpeed * Time.deltaTime;
+			mWalking = true;
+		}
+
+		if (Input.GetKey (KeyCode.D) == false && Input.GetKey (KeyCode.A) == false)
+			mWalking = false;
+		
+		if (Input.GetKeyDown (KeyCode.Space) && mGrounded)
+			mRigidBody2D.AddForce(new Vector2(0.0f, 1.0f) * mJumpForce);
+				
+	}
+
+	void Jump ()
+	{
+		if (mGrounded && Input.GetButtonDown("Jump"))
+		{
+			mRigidBody2D.AddForce(new Vector2(0.0f, 1.0f) * mJumpForce);
+		}
+	}
+	
+	void CheckFalling()
+    {
+        mFalling = mRigidBody2D.velocity.y < 0.0f;
+		mAnimator.SetBool("IsFalling", mFalling);
+    }
+
+	void FaceDirection(Vector2 direction)
+	{
+        Quaternion rotation3D = direction == Vector2.right ? Quaternion.LookRotation(Vector3.forward) : Quaternion.LookRotation(Vector3.back); 
+        mSpriteChild.rotation = rotation3D;
+	}
+
+	void CheckGrounded()
+	{
+		mGrounded = Physics2D.OverlapArea(mTopLeftPosition.position, mBottomRightPosition.position, mGroundLayer); 
+		mAnimator.SetBool("IsGrounded", mGrounded);
+	}
+
+}
---
+++ Assets/Scripts/Environment/LeverSwitch.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverSwitch : MonoBehaviour {
+    public GameObject mLinkedDoor;
+
+    public void ActivateSwitch()
+    {
+        GetComponent<Animator>().enabled = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ActivateSwitch();
+    }
+}
