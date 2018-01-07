cf2075ced07c9d744dfde13c68d3d58fba9edc0a
NAD.QC.CA\xpro <steven.dien@outlook.com>
2018-01-07 16:19:41 -0500

    Quick text fix in stage 3

--- Assets/Scripts/Characters/EnemyController.cs
+++ Assets/Scripts/Characters/EnemyController.cs
@@ -151,7 +151,7 @@ public class EnemyController : MonoBehaviour
 		GameObject.Find ("DialogueText").GetComponent<Text> ().text = "Hoho… On dirait que j’ai parlé trop vite… Il nous reste un dernier combat apparemment !";
 		audioSources[1].Play();
 		mHealthPoints = mBossHP;
-		yield return new WaitForSeconds (1f);
+		yield return new WaitForSeconds (5f);
 		GameObject.Find ("DialogueText").GetComponent<Text> ().text = "";
 		mDead = false;
 	}
