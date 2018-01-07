ecf782f6b6233af6e0de5cdd0c5de522b8b0eee7
NAD.QC.CA\xpro <steven.dien@outlook.com>
2018-01-07 09:15:45 -0500

    - Fixed the cursor / fireball movement in the menu scene

--- Assets/Scripts/UI/MenuController.cs
+++ Assets/Scripts/UI/MenuController.cs
@@ -6,7 +6,6 @@ using UnityEngine;
 
 public class MenuController : MonoBehaviour
 {
-	public Canvas mQuitMenu;
 	public Button mStartGameButton;
 	public Button mExitButton;
 
@@ -14,21 +13,6 @@ public class MenuController : MonoBehaviour
 	{
 		mStartGameButton = mStartGameButton.GetComponent<Button> ();
 		mExitButton = mExitButton.GetComponent<Button> ();
-		mQuitMenu.enabled = false;
-	}
-
-	public void ExitPress()
-	{
-		mQuitMenu.enabled = true;
-		mStartGameButton.enabled = false;
-		mExitButton.enabled = false;
-	}
-
-	public void NoPress()
-	{
-		mQuitMenu.enabled = false;
-		mStartGameButton.enabled = true;
-		mExitButton.enabled = true;
 	}
 
 	public void StartGame()
---
+++ Assets/Scripts/UI/MenuCursor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCursor : MonoBehaviour
+{
+	public float speed;
+
+	void Awake()
+	{
+		Cursor.visible = false;
+	}
+
+	void Update ()
+	{
+		Move ();
+	}
+
+	void Move()
+	{
+		Vector3 mouselocation = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		float xDist = mouselocation.x - transform.position.x;
+		float yDist = mouselocation.y - transform.position.y;
+
+		transform.Translate(new Vector3(xDist, yDist) * Time.deltaTime * speed);
+	}
+}
